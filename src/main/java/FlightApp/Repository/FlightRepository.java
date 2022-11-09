2dad97056d85dad028b2da9f0cd452d9abc3433e
qiukyra17 <kyraqiu@gmail.com>
2022-11-09 17:51:20 -0500

    restrutured model

--- src/main/java/FlightApp/Repository/FlightRepository.java
+++ src/main/java/FlightApp/Repository/FlightRepository.java
@@ -11,7 +11,7 @@ public interface FlightRepository extends JpaRepository<Flight, Integer> {
     @Query("FROM Flight WHERE departure_city_id= :departureCity")
     List<Flight> getAllFlightsByDepartureCityId(@Param("departureCity") int departureCityId);
 
-    @Query("FROM Flight WHERE departureCity= :departureCity AND arrivalCity= :arrivalCity")
+    @Query("FROM Flight WHERE departure_city_id= :departureCity AND arrival_city_id= :arrivalCity")
     List<Flight> getAllFlightsByDeptAndArr(@Param("departureCity") int departureCityId,
                                            @Param("arrivalCity") int arrivalCityId);
 
