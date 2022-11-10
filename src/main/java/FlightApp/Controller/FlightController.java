c4a1514a9724a95cac186da184c17ae4068e3a20
qiukyra17 <kyraqiu@gmail.com>
2022-11-10 10:42:38 -0500

    cross origin added

--- src/main/java/FlightApp/Controller/FlightController.java
+++ src/main/java/FlightApp/Controller/FlightController.java
@@ -3,13 +3,11 @@ package FlightApp.Controller;
 import FlightApp.Model.Flight;
 import FlightApp.Service.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("flights")
 public class FlightController {
